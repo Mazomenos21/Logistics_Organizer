51b53b3c855229dda20329592c287b3e61654b72
Tatiana López García <92939941+tatilopeg@users.noreply.github.com>
2023-09-04 12:39:27 -0500

    Tatiana

--- main/src/Estado.jsx
+++ main/src/Estado.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import './Personal.css';
+import './styles/Personal.css';
 import imagen1 from './imagen1.jpg';
 
 
